5a4a812c9c06b9bb221bb7e2e644428de4a5a4b2
윤효정 <0213yhj@naver.com>
2024-10-24 14:50:31 +0900

    Chore(config):버전에 맞게 수정

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone', // standalone 모드 활성화
+};
+
+module.exports = nextConfig;
