4883f423ee38446afae38e47bbd3d90eded24e84
carbancle <carbancle2@gmail.com>
2024-10-25 17:48:19 +0900

    Desgin(Navbar): 누락된 수정 사항 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,8 @@ import NavbarBtn from "./NavbarBtn";
 
 const Navbar = () => {
   return (
-    <div className="fixed bottom-0 py-2.5 px-5 w-full h-[83px] left-1/2 transform -translate-x-1/2 bg-gray-100 border-t border-[#D9D9D9] max-w-[800px] mx-auto">
-      <div className="flex justify-around items-center">
+    <div className="fixed bottom-0 py-2.5 w-full h-[83px] left-1/2 transform -translate-x-1/2 bg-gray-100 border-t border-[#D9D9D9] max-w-[800px] mx-auto">
+      <div className="flex px-5 justify-between items-center">
         <NavbarBtn btnName="카테고리" imgName="Menu" path="/category" />
         <NavbarBtn btnName="팔래요" imgName="Edit" path="/sell" />
         <NavbarBtn btnName="홈" imgName="Home" path="/" />
