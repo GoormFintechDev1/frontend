6f7d0e83233514c8e820e7296bd9fa4ccda58d9c
carbancle <carbancle2@gmail.com>
2024-10-25 12:43:21 +0900

    Desgin(Navbar): 네비게이션 바 디자인 작업 진행

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import QueryProvider from "@/Provider/QueryProvider";
 import { AuthProvider } from "@/Provider/AuthProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <QueryProvider>
           <AuthProvider>
             {children}
+            <Navbar />
           </AuthProvider>
         </QueryProvider>
       </body>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,36 @@
+"use client";
+
+import Image from "next/image"
+import { usePathname } from "next/navigation"
+
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
-    <div>Navbar</div>
+    <div className="fixed bottom-0 w-full left-1/2 transform -translate-x-1/2 bg-gray-100 border-t border-[#D9D9D9] max-w-md mx-auto">
+      <div className="flex justify-around items-center h-16">
+        <div className="flex flex-col items-center">
+          <Image src="/nav/Menu.png" alt="카테고리" width={24} height={24} />
+          <span className="text-sm text-gray-700">카테고리</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src="/nav/Edit.png" alt="팔래요" width={24} height={24} />
+          <span className="text-sm text-gray-700">팔래요</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src="/nav/Home.png" alt="홈" width={24} height={24} />
+          <span className="text-sm text-gray-700">홈</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src="/nav/Heart.png" alt="관심상품" width={24} height={24} />
+          <span className="text-sm text-gray-700">관심상품</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image src="/nav/User.png" alt="마이" width={24} height={24} />
+          <span className="text-sm text-gray-700">마이</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
