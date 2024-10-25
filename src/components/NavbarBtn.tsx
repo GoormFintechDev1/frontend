6b1be50811eeddedfecf48943a8424a44de13598
carbancle <carbancle2@gmail.com>
2024-10-25 17:47:32 +0900

    Desgin(Navbar): hover 액션 수정, 여백을 시안과 일치하도록 구현

--- src/components/NavbarBtn.tsx
+++ src/components/NavbarBtn.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation"
 interface BtnProps {
   path: string,
   btnName: string,
-  imgPath: string,
+  imgName: string,
 }
 
 const NavbarBtn = (props: BtnProps) => {
@@ -15,13 +15,13 @@ const NavbarBtn = (props: BtnProps) => {
   const pathname = usePathname();
   return (
     <button 
-      className={`flex flex-col items-center p-2 rounded-lg ${
-        pathname === `${props.path}` ? 'bg-[#34D399]' : 'bg-transparent'
-      }`}
+      className={`flex flex-col justify-center items-center w-[50px] h-[50px] rounded-lg`}
       onClick={() => router.push(`${props.path}`)}
     >
-      <Image src={`${props.imgPath}`} alt={`${props.btnName}`} width={24} height={24} />
-      <span className={`text-sm ${pathname === `${props.path}` ? 'text-white' : 'text-gray-700'}`}>
+      <Image
+      src={pathname === props.path ? `/nav/${props.imgName}_on.png` : `/nav/${props.imgName}.png`}
+      alt={`${props.btnName}`} width={24} height={24} />
+      <span className={`text-xs ${pathname === `${props.path}` ? 'text-emerald-400' : 'text-gray-700'}`}>
         {props.btnName}
       </span>
     </button>
