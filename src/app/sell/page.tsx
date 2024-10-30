b22b708d4b881568a100b24951b8fab539a8fde1
윤효정 <0213yhj@naver.com>
2024-10-30 16:47:14 +0900

    Design(sell): 상품 등록 페이지 수정

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -60,13 +60,13 @@ export default function Sell() {
 
     return (
         <div className="flex flex-col space-y-8 h-full w-full">
-            <div className="flex flex-row justify-between">
+            <div className="flex justify-between items-center w-full p-4 fixed top-0 left-0 bg-white">
                 <Image alt="취소하기" src="/icons/X.png" width={25} height={25}></Image>
                 <p className="main-text">내 상품 판매하기</p>
                 <span className="w-6"></span>
                 {/* <Image alt="취소하기" src="/icons/Done.png" width={25} height={25}></Image> */}
             </div>
-            <form className="h-full space-y-6">
+            <form className="flex flex-col overflow-y-auto pb-20 space-y-6">
                 <div>
                     <label className="label-sm">이미지 추가</label>
                     <div>이미지 추가 기능 구현 예정...</div>
@@ -84,8 +84,8 @@ export default function Sell() {
                 </div>
                 <div className="label-input-set">
                     <label className="label-sm">가격</label>
-                    <div className="flex flex-row space-x-2 !h-12 !rounded-md input-base text-sm input-base">
-                        <input className="placeholder:text-xs w-full h-full" placeholder="가격을 입력하세요."></input>
+                    <div className="flex flex-row space-x-2 !h-12 !rounded-md input-base text-sm input-base focus-within:border-emerald-400">
+                        <input className="placeholder:text-xs w-full h-full focus:outline-none" placeholder="가격을 입력하세요." />
                         <p>원</p>
                     </div>
                 </div>
@@ -95,13 +95,13 @@ export default function Sell() {
                 </div>
                 <div className="label-input-set">
                     <label className="label-sm">상세 설명</label>
-                    <textarea className="text-sm !h-32 !rounded-md border-[#d9d9d9] placeholder:text-[10px] placeholder:leading-5 p-3 border placeholder:text-gray-600" placeholder={textGuide}></textarea>
+                    <textarea className="text-sm !h-32 !rounded-md border-[#d9d9d9] placeholder:text-[10px] placeholder:leading-5 p-3 border placeholder:text-gray-600 focus:border-emerald-400 outline-none" placeholder={textGuide}></textarea>
                 </div>
                 <div className="label-input-set">
                     <label className="label-sm">거래 희망 장소</label>
                     <Select styles={customStyles} placeholder="장소를 선택하세요." options={categoryOptions}></Select>
                 </div>
-                <div>
+                <div className="fixed bottom-0 left-0 w-full px-8 py-5 bg-white shadow-md">
                     <Button>등록하기</Button>
                 </div>
             </form>
