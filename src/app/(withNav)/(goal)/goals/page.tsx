a7a06fd291fd2d656ddada08e8404ce8375e5597
sujin <sujin7048@naver.com>
2024-11-18 12:00:59 +0900

    feat(goal): 목표달성뱃지 구현

--- src/app/(withNav)/(goal)/goals/page.tsx
+++ src/app/(withNav)/(goal)/goals/page.tsx
@@ -25,7 +25,6 @@ export default function Objective() {
     const date = paramMonth2(year, currentIndex);
 
     const {data: revenue} = useRevenueGoal(date);
-
     const {data: expense} = useExpenseGoal(date);
 
     const revenuePercentage = Math.round((revenue?.monthlyRevenue0Ago / revenue?.revenueGoal0Ago) * 100);
@@ -43,82 +42,99 @@ export default function Objective() {
                 <Image alt="back" src={'/icons/arrow.png'} width={25} height={25}></Image>
                 </Link>
             </div>
-            <div className="mb-8">
-                <h1 className="text-xl font-semibold">목표를 관리해보세요!</h1>
-            </div>
 
-                {/* 매출 목표 */}
-            <Link href="/goals/detail?page=revenue">
-                <div className="bg-white rounded-lg shadow p-4 mb-4 cursor-pointer">
-                    <p className="text-base font-medium mb-2">매출 목표</p>
-                    <div className="flex items-center">
-                        <div className="w-1/2 flex justify-center items-center">
-                            <PieChart width={95} height={100}>
-                                <Pie
-                                    data={revenueData}
-                                    cx="50%"
-                                    cy="50%"
-                                    innerRadius={30}
-                                    outerRadius={40}
-                                    dataKey="value"
-                                    startAngle={90}
-                                    endAngle={-270}
-                                >
-                                    {revenueData.map((entry, index) => (
-                                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                                    ))}
-                                    <Label
-                                        value={`${Math.min(revenuePercentage, 100)}%`} 
-                                        position="center"
-                                        style={labelStyle}
-                                    />
-                                </Pie>
-                            </PieChart>
-                        </div>
-                        <div className="w-1/2 text-center">
-                            <span className="text-gray-500 text-lg">목표 금액</span>
-                            <p className="text-emerald-500 text-xl font-semibold">{convertToKoreanWon(revenue?.revenueGoal0Ago)} </p>
-                        </div>
+            <div className="mb-6">
+            <h1 className="text-xl font-semibold ">목표를 관리해보세요 !</h1>
+            <h2 className="text-lg font-semibold mt-3"> ⭐ 목표를 달성하고 배지를 모아보세요 ⭐</h2>
+                <div className="bg-white rounded-lg shadow-xl p-6 mt-3">
+                    <div className="grid grid-cols-6 gap-4">
+                        {[...Array(12)].map((_, index) => (
+                            <div
+                                key={index}
+                                className={`w-16 h-16 flex items-center justify-center rounded-full border-2 ${
+                                    index === 10 ? "border-amber-300 bg-amber-50 " : "border-gray-100"}`}>
+                                {index === 10 && 
+                                <Image alt="badge" src={`/icons/medal.png`} width={50} height={50} className="text-4xl"></Image>}
+                            </div>
+                        ))}
                     </div>
                 </div>
-                </Link>
+            </div>
 
-
-                {/* 지출 목표 */}
-                <Link href="/goals/detail?page=expense">
-                    <div className="bg-white rounded-lg shadow p-4 mt-10 cursor-pointer">
-                        <p className="text-base font-medium mb-10">지출 예산</p>
-                        <div className="flex items-center mb-4">
-                            <div className="w-1/2 flex flex-col justify-center items-center">
-                                <p className="text-gray-600">지출 예산보다</p>
-                                {expense?.monthlyExpense0Ago && expense?.monthlyExpense1Ago ? (
-                                    expense.expenseGoal0Ago < expense.monthlyExpense1Ago ? (
-                                        <p className="text-red-600 text-lg font-semibold">
-                                            {convertToKoreanWon(Math.abs(expense?.expenseGoal0Ago - expense?.monthlyExpense0Ago))} 더 썼어요.
-                                        </p>
-                                    ) : (
-                                        <p className="text-blue-600 text-lg font-semibold">
-                                            {convertToKoreanWon(Math.abs(expense?.monthlyExpense1Ago - expense?.expenseGoal0Ago))} 덜 썼어요.
-                                        </p>
-                                    )
-                                ) : (
-                                    <p>데이터가 없습니다.</p>
-                                )}
+            <div className="mt-14">
+                {/* 매출 목표 */}
+                <Link href="/goals/detail?page=revenue">
+                    <div className="bg-white rounded-lg shadow-lg p-4 cursor-pointer">
+                        <p className="text-lg mb-2">매출 목표</p>
+                        <div className="flex items-center">
+                            <div className="w-1/2 flex justify-center items-center">
+                                <PieChart width={100} height={100}>
+                                    <Pie
+                                        data={revenueData}
+                                        cx="50%"
+                                        cy="50%"
+                                        innerRadius={30}
+                                        outerRadius={40}
+                                        dataKey="value"
+                                        startAngle={90}
+                                        endAngle={-270}
+                                    >
+                                        {revenueData.map((entry, index) => (
+                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                        ))}
+                                        <Label
+                                            value={`${Math.min(revenuePercentage, 100)}%`} 
+                                            position="center"
+                                            style={labelStyle}
+                                        />
+                                    </Pie>
+                                </PieChart>
                             </div>
                             <div className="w-1/2 text-center">
-                                <span className="text-gray-500 text-lg">소비 금액</span>
-                                <p className="text-gray-600 text-xl font-semibold">
-                                    {convertToKoreanWon(expense?.monthlyExpense0Ago)}
-                                </p>
+                                <span className="text-gray-500 text-lg">목표 금액</span>
+                                <p className="text-emerald-500 text-xl font-semibold">{convertToKoreanWon(revenue?.revenueGoal0Ago)} </p>
                             </div>
                         </div>
                     </div>
-                </Link>
+                    </Link>
+
+
+                    {/* 지출 목표 */}
+                    <Link href="/goals/detail?page=expense">
+                        <div className="bg-white rounded-lg shadow-lg p-4 mt-10 cursor-pointer">
+                            <p className="text-lg font-medium mb-10">지출 예산</p>
+                            <div className="flex items-center mb-4">
+                                <div className="w-1/2 flex flex-col justify-center items-center">
+                                    <p className="text-gray-600">지출 예산보다</p>
+                                    {expense?.monthlyExpense0Ago && expense?.monthlyExpense1Ago ? (
+                                        expense.expenseGoal0Ago < expense.monthlyExpense1Ago ? (
+                                            <p className="text-red-600 text-lg font-semibold">
+                                                {convertToKoreanWon(Math.abs(expense?.expenseGoal0Ago - expense?.monthlyExpense0Ago))} 더 썼어요.
+                                            </p>
+                                        ) : (
+                                            <p className="text-blue-600 text-lg font-semibold">
+                                                {convertToKoreanWon(Math.abs(expense?.monthlyExpense0Ago - expense?.expenseGoal0Ago))} 덜 썼어요.
+                                            </p>
+                                        )
+                                    ) : (
+                                        <p>데이터가 없습니다.</p>
+                                    )}
+                                </div>
+                                <div className="w-1/2 text-center">
+                                    <span className="text-gray-500 text-lg">소비 금액</span>
+                                    <p className="text-gray-600 text-xl font-semibold">
+                                        {convertToKoreanWon(expense?.monthlyExpense0Ago)}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+            </div>
         </div>
     );
 }
 
-function calculateRemainingBudgetPercentage(currentExpense:number, budget:number) {
-    if (budget === 0) return 0; // 예산이 0일 경우 남은 비율 0%로 처리
-    return Math.round((1 - currentExpense / budget) * 100);
-}
+// function calculateRemainingBudgetPercentage(currentExpense:number, budget:number) {
+//     if (budget === 0) return 0; // 예산이 0일 경우 남은 비율 0%로 처리
+//     return Math.round((1 - currentExpense / budget) * 100);
+// }
