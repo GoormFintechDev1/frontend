9b6b3cd88f28be49142b7daa1554b06dd7fd2124
윤효정 <0213yhj@naver.com>
2024-11-19 17:45:11 +0900

    Feat(report)

--- src/components/reportlist/ReportIndusty.tsx
+++ src/components/reportlist/ReportIndusty.tsx
@@ -37,67 +37,67 @@ export default function ReportIndusty({report}:Props) {
             <p className="font-bold text-sm pb-3">매출/지출 비교</p>
             <div className="text-xs" style={{ color: "#333333" }}>
             <div className="flex justify-around">
-  {/* 매출 차트 */}
-  <div className="flex flex-col items-center space-y-2">
-    <p className="text-sm font-medium">매출</p>
-    <ResponsiveContainer width={120} height={140}>
-      <BarChart data={revenueChart} margin={{ top: 0, bottom: 5 }}>
-        <XAxis dataKey="name" hide />
-        <YAxis hide />
-        <Tooltip formatter={(value: number) => `${value}만원`} />
-        <Bar
-          dataKey="평균"
-          fill="#CECECE"
-          barSize={45}
-          radius={[10, 10, 10, 10]}
-          label={{ position: "insideTop", fill: "#666", fontSize: 12 }}
-        />
-        <Bar
-          dataKey="나"
-          fill="#92B4FF"
-          barSize={45}
-          radius={[10, 10, 10, 10]}
-          label={{ position: "insideTop", fill: "#fff", fontSize: 12 }}
-        />
-      </BarChart>
-    </ResponsiveContainer>
-    <div className="flex justify-between w-full text-xs">
-      <div className="text-center w-1/2 text-gray-500">평균</div>
-      <div className="text-center w-1/2 text-blue-500">나</div>
-    </div>
-  </div>
+            {/* 매출 차트 */}
+            <div className="flex flex-col items-center space-y-2">
+                <p className="text-sm font-medium">매출</p>
+                <ResponsiveContainer width={120} height={120}>
+                <BarChart data={revenueChart} margin={{ top: 0, bottom: 5 }}>
+                    <XAxis dataKey="name" hide />
+                    <YAxis hide />
+                    {/* <Tooltip formatter={(value: number) => `${value}만원`} /> */}
+                    <Bar
+                    dataKey="평균"
+                    fill="#CECECE"
+                    barSize={45}
+                    radius={[10, 10, 10, 10]}
+                    label={{ position: "insideTop", fill: "#666", fontSize: 12 }}
+                    />
+                    <Bar
+                    dataKey="나"
+                    fill="#92B4FF"
+                    barSize={45}
+                    radius={[10, 10, 10, 10]}
+                    label={{ position: "insideTop", fill: "#fff", fontSize: 12 }}
+                    />
+                </BarChart>
+                </ResponsiveContainer>
+                <div className="flex justify-between w-full text-xs">
+                <div className="text-center w-1/2 text-gray-500">평균</div>
+                <div className="text-center w-1/2 text-blue-500">나</div>
+                </div>
+            </div>
 
-  {/* 지출 차트 */}
-  <div className="flex flex-col items-center space-y-2">
-    <p className="text-sm font-medium">지출</p>
-    <ResponsiveContainer width={120} height={140}>
-      <BarChart data={expenseChart} margin={{ top: 0, bottom: 5 }}>
-        <XAxis dataKey="name" hide />
-        <YAxis hide />
-        <Tooltip formatter={(value: number) => `${value}만원`} />
-        <Bar
-          dataKey="나"
-          fill="#d3d3d3"
-          barSize={45}
-          radius={[10, 10, 10, 10]}
-          label={{ position: "insideTop", fill: "#666", fontSize: 12 }}
-        />
-        <Bar
-          dataKey="평균"
-          fill="#f76c6c"
-          barSize={45}
-          radius={[10, 10, 10, 10]}
-          label={{ position: "insideTop", fill: "#fff", fontSize: 12 }}
-        />
-      </BarChart>
-    </ResponsiveContainer>
-    <div className="flex justify-between w-full text-xs">
-      <div className="text-center w-1/2 text-gray-500">평균</div>
-      <div className="text-center w-1/2 text-red-500">나</div>
-    </div>
-  </div>
-</div>
-</div>
+            {/* 지출 차트 */}
+            <div className="flex flex-col items-center space-y-2">
+                <p className="text-sm font-medium">지출</p>
+                <ResponsiveContainer width={120} height={120}>
+                <BarChart data={expenseChart} margin={{ top: 0, bottom: 5 }}>
+                    <XAxis dataKey="name" hide />
+                    <YAxis hide />
+                    {/* <Tooltip formatter={(value: number) => `${value}만원`} /> */}
+                    <Bar
+                    dataKey="나"
+                    fill="#d3d3d3"
+                    barSize={45}
+                    radius={[10, 10, 10, 10]}
+                    label={{ position: "insideTop", fill: "#666", fontSize: 12 ,}}
+                    />
+                    <Bar
+                    dataKey="평균"
+                    fill="#f76c6c"
+                    barSize={45}
+                    radius={[10, 10, 10, 10]}
+                    label={{ position: "insideTop", fill: "#fff", fontSize: 12 }}
+                    />
+                </BarChart>
+                </ResponsiveContainer>
+                <div className="flex justify-between w-full text-xs">
+                <div className="text-center w-1/2 text-gray-500">평균</div>
+                <div className="text-center w-1/2 text-red-500">나</div>
+                </div>
+            </div>
+            </div>
+            </div>
         </div>
         <div className="text-base" style={{color:"#333333", lineHeight:"28px"}}>
             <div className="py-3">
--- src/components/reportlist/ReportMarket.tsx
+++ src/components/reportlist/ReportMarket.tsx
@@ -17,15 +17,21 @@ export default function ReportMarket({report}: Props) {
     console.log(report)
   return (
     <div>
-        <div className="text-xl font-bold">시장 동향</div>
+        <div className="text-xl font-bold py-3">시장 동향</div>
+        <div className="flex w-full space-x-5">
+            <div className="min-h-7 w-1/2 rounded-xl p-5 my-2 bg-zinc-50 items-center">
+                <p className="font-bold text-sm pb-3 ">BSI 지수</p>
+                <div className="text-base h-10 justify-self-center" style={{color:"#333333"}}><p className="font-extrabold text-emerald-400">{report?.BSI_index}</p></div>
+            </div>
+            <div className="min-h-7 w-1/2 rounded-xl p-5 my-2 bg-zinc-50">
+                <p className="font-bold text-sm pb-3 ">환율</p>
+                <div className="text-base h-10 justify-self-center items-center" style={{color:"#333333"}}><p className="font-extrabold text-emerald-400">{report?.exchange_rate} 원</p></div>
+            </div>
+        </div>
         <div className="text-base" style={{color:"#333333", lineHeight:"28px"}}>
             <div className="py-3">
                 <p className="font-bold">BSI</p>
                 <div>{report?.BSI_description}</div>
-            </div>
-            <div className="py-3">
-                <p className="font-bold">환율</p>
-                <div>{report?.exchange_rate}</div>
             </div>
             <div className="py-3">
                 <p className="font-bold">원가</p>
@@ -37,15 +43,15 @@ export default function ReportMarket({report}: Props) {
             </div>
             <div className="py-3">
                 <p className="font-bold">추천</p>
-                <div>{report?.recommendations.map((r, i)=>(<div key={i}>
-                    <li>{r}</li>
-                </div>))}</div>
+                <ul className="ml-5 list-outside list-disc space-y-2">{report?.recommendations.map((r, i)=>(
+                    <li key={i}>{r}</li>
+                ))}</ul>
             </div>
         </div>
-        <div className="min-h-7 rounded-xl p-5" style={{backgroundColor:"#FAFAFA"}}>
+        {/* <div className="min-h-7 rounded-xl p-5" style={{backgroundColor:"#FAFAFA"}}>
             <p className="font-bold text-sm pb-3">매출/지출 비교</p>
             <div className="text-xs" style={{color:"#333333"}}>차트영역...</div>
-        </div>
+        </div> */}
     </div>
   )
 }
