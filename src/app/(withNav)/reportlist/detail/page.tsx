7b768a45056cd036960e8c368fb9f9ab8e192607
윤효정 <0213yhj@naver.com>
2024-11-19 18:01:08 +0900

    Style(report)

--- src/app/(withNav)/reportlist/detail/page.tsx
+++ src/app/(withNav)/reportlist/detail/page.tsx
@@ -1,12 +1,8 @@
 "use client"
 
-// import ReportItem from '@/components/reportlist/ReportItem';
 import { useReportQuery2 } from '@/hooks/useReportQuery';
-import useReportsStore from '@/stores/useReportsStore';
-// import { handleNextMonth, handlePrevMonth } from '@/utils/calculateDay';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
-// import { useState } from 'react';
 import dayjs from "dayjs";
 import { useUserInfo } from '@/hooks/useUserQuery';
 import ReportIndusty from '@/components/reportlist/ReportIndusty';
@@ -17,13 +13,7 @@ dayjs().format();
 export default function ReportDetail() {
     const searchParams = useSearchParams();
     const initialMonth = searchParams.get('month') || '2024-11'; // 기본값을 11월로 설정
-    // const [month, setMonth] = useState(initialMonth);
     const month = initialMonth;
-
-    // useReportQuery(month);
-
-    // const reportData = useReportsStore((state) => state.data);
-
     
     const {data: useInfo} = useUserInfo();
     const {data: reportData} = useReportQuery2(month);
@@ -54,21 +44,11 @@ export default function ReportDetail() {
             
             <div className=" mt-4">
                 <div className="flex items-left space-x-4 ">
-                    {/* <button onClick={() => setMonth(handlePrevMonth(month))} className="text-xl font-semibold px-2">
-                        <Image src={"/icons/Back.png"} alt="PrevMonth" width={24} height={24} />
-                    </button> */}
                     <h1 className="text-xl font-bold pb-4 "><span>{useInfo?.companyName}</span> {month.slice(-2)}월 리포트</h1>
-                    {/* <button onClick={() => setMonth(handleNextMonth(month))} className="text-xl font-semibold px-2">
-                        <Image src={"/icons/Forward.png"} alt="NextMonth" width={24} height={24} />
-                    </button> */}
                 </div>
             </div>
 
             <div className="pt-4 overflow-scroll flex flex-col space-y-10 " style={{height}}> {/* 높이 변경 필요... */}
-                {/* {reportData.map((item, index) => (
-                    <ReportItem key={index} item={item} />
-                ))} */}
-                {/* <div>{reportData.reports}</div> */}
                 <ReportIndusty report={reportData?.reports.INDUSTRY_REPORT}/>
                 <ReportMarket report={reportData?.reports.MARKET_REPORT}/>
             </div>
