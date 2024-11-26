5b6ebc9dfc19b6c14de3198f55c59eb616b93018
sujin <sujin7048@naver.com>
2024-11-26 16:23:06 +0900

    feat(card, mypage): 마이페이지 수정, 카드추천페이지 구현

---
+++ src/app/(withNav)/mpedit/page.tsx
@@ -0,0 +1,122 @@
+"use client"
+
+import { useCheckPhoneNumber } from "@/hooks/useAuthQuery";
+import { InputType2 } from "@/interface/register";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface Props {
+    onReadySubmit: (data: InputType2) => void;
+    onPrev: () => void;
+  }
+  
+  export default function Mpedit({} : Props) {
+    const {
+      register,
+      watch,
+      clearErrors,
+      setError,
+      formState: { errors },
+    } = useForm<InputType2>({ mode: "onChange" });
+  
+    const [isPhoneNumberAvailable, setIsPhoneNumberAvailable] = useState("");
+    const [isPhoneNumberChecked, setIsPhoneNumberChecked] = useState(false);
+  
+    const phoneNumber = watch("phoneNumber");
+    const checkPhoneNumberMutation = useCheckPhoneNumber();
+  
+    useEffect(() => {
+      // 전화번호가 변경될 때 상태 초기화
+      if (phoneNumber !== "") {
+        setIsPhoneNumberChecked(false);
+        setIsPhoneNumberAvailable("");
+        clearErrors("phoneNumber");
+      }
+    }, [phoneNumber, clearErrors]);
+  
+    const handleCheckPhoneNumber = () => {
+      if (phoneNumber === "" || errors.phoneNumber) return;
+  
+      clearErrors("phoneNumber");
+      checkPhoneNumberMutation.mutate(phoneNumber, {
+        onSuccess: (data) => {
+          if (data) {
+            setError("phoneNumber", {
+              type: "manual",
+              message: "이미 가입된 전화번호입니다.",
+            });
+            setIsPhoneNumberChecked(false);
+          } else {
+            setIsPhoneNumberAvailable("사용 가능한 전화번호입니다.");
+            setIsPhoneNumberChecked(true);
+          }
+        },
+        onError: () => {
+          setError("phoneNumber", {
+            type: "manual",
+            message: "전화번호 중복 검사 중 문제가 발생했습니다.",
+          });
+          setIsPhoneNumberChecked(false);
+        },
+      });
+    };
+
+
+    return (
+        <div className="container">
+            <div className="back">
+                <Link href="/mypage">
+                <Image alt="back" src={'/icons/arrow.png'} width={25} height={25}></Image>
+                </Link>
+            </div>
+
+            <div className="p-4">
+                <h1 className="text-xl font-bold">내 정보 수정</h1>
+            </div>
+
+            <div className="mb-4 p-3">
+                <label className="block text-lg font-bold mb-2 ml-1">전화번호</label>
+                <div className="flex items-center gap-3">
+                <input
+                    type="text"
+                    placeholder="전화번호를 입력하세요."
+                    className="input-base flex-grow border rounded-lg p-2"
+                    {...register("phoneNumber", {
+                    required: "전화번호를 입력하세요.",
+                    pattern: {
+                        value: /^[0-9]{10,11}$/,
+                        message: "유효한 전화번호를 입력해주세요.",
+                    },
+                    })}
+                />
+                <button
+                    type="button"
+                    className={`p-4 rounded-lg text-md  ${
+                    isPhoneNumberChecked
+                        ? "bg-gray-300 text-gray-500 cursor-not-allowed"
+                        : "bg-emerald-500 text-white"
+                    }`}
+                    disabled={isPhoneNumberChecked}
+                    onClick={handleCheckPhoneNumber}
+                >
+                    중복 확인
+                </button>
+                </div>
+
+                {/* 에러 메시지 */}
+                {errors.phoneNumber && (
+                <p className="text-red-500 text-sm mt-2">
+                    {errors.phoneNumber.message}
+                </p>
+                )}
+                {!errors.phoneNumber && isPhoneNumberAvailable && (
+                <p className="text-emerald-500 text-sm mt-2">
+                    {isPhoneNumberAvailable}
+                </p>
+                )}
+            </div>
+        </div>
+    );
+}
--- src/app/(withNav)/mypage/page.tsx
+++ src/app/(withNav)/mypage/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
-import { useLogoutMutation } from "@/hooks/useAuthQuery";
+import { useLogoutMutation } from "@/hooks/useAuthQuery"; 
 import { useUserInfo } from "@/hooks/useUserQuery";
 import dayjs from "dayjs";
+import Link from "next/link";
+import { useState } from "react";
 dayjs().format();
 
 
@@ -10,35 +12,74 @@ export default function MyPage() {
 
     const {data: useInfo} = useUserInfo();
 
-    const logout = useLogoutMutation();
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const hanldeLogout = () => {
+    const openModal = () => setIsModalOpen(true);
+    const closeModal = () => setIsModalOpen(false);
+
+    const logout = useLogoutMutation();
+    const handleLogout = () => {
         logout.mutate(useInfo?.loginId);
     }
 
     return (
-        <div className="container flex flex-col h-screen">
-            <div className="flex items-center justify-between ">
-                <h1 className="text-xl ml-4 mt-4 p-2 font-extralight"> 📌{useInfo?.companyName} </h1>
+        <div className="container ">
+            <div className="items-center border-b">
+                    <h1 className="text-xl ml-4 mt-4 p-2 font-semibold"> 📌{useInfo?.companyName} </h1>
+                    <h1 className="font-extralight ml-6 mt-2 text-lg">📍{useInfo?.phoneNumber}</h1>
+                    <h1 className="font-extralight ml-6 mt-2 text-lg mb-4">📍{useInfo?.address}</h1>
             </div>
 
-            <div className="text-right mr-4">
-                <p className="text-gray-500 border-b" onClick={hanldeLogout}> 로그아웃 </p>
+            <div className="p-5 border-b flex justify-between items-center">
+            <Link href="/mpedit" className="flex justify-between w-full items-center">
+                <p className="text-lg">내 정보 수정</p>
+                <span className="ml-auto">&gt;</span>
+            </Link>
             </div>
 
-            <div className="flex flex-col p-6 m-6 bg-gray-50 rounded-lg shadow-md">
-                <p className="font-extralight text-xl">🔗이름 </p>
-                <h1 className="font-bold ml-6 mt-2 text-lg">📍{useInfo?.name}</h1>
+            {/* <div className="p-5 border-b flex justify-between items-center">
+                <p className="text-lg ">사업자 인증 </p>
+                <span className="ml-auto">&gt;</span>
+            </div> */}
+
+            <div className="p-5 border-b flex justify-between items-center">
+                <p className="text-lg">알림 </p>
+                <span className="text-end">&gt;</span>
             </div>
 
-            <div className="flex flex-col p-6 m-6 bg-gray-50 rounded-lg shadow-md">
-                <p className="font-extralight text-xl">📞전화번호</p>
-                <h1 className="font-bold ml-6 mt-2 text-lg">📍{useInfo?.phoneNumber}</h1>
+            <div className="p-5 border-b flex justify-between items-center" onClick={openModal}>
+                <p className="text-lg "> 로그아웃 </p>
+                <span className="text-end">&gt;</span>
             </div>
 
-            <div className="flex flex-col p-6 m-6 bg-gray-50 rounded-lg shadow-md ">
-                <p className="font-extralight text-xl">🏠주소 </p>
-                <h1 className="font-bold ml-6 mt-2 text-lg">📍{useInfo?.address}</h1>
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50">
+                    <div className="bg-white p-6 rounded-lg shadow-lg w-80">
+                        <p className="text-lg font-bold mb-4 text-center">
+                        정말 로그아웃 하시겠습니까?
+                        </p>
+                        <p className="text-sm text-gray-500 mb-6 text-center">추가 메시지</p>
+                        <div className="flex gap-4 justify-center">
+                        <button
+                            className="px-4 py-2 bg-emerald-300 rounded-lg"
+                            onClick={handleLogout}
+                        >
+                            예
+                        </button>
+                        <button
+                            className="px-4 py-2 bg-gray-300 rounded-lg"
+                            onClick={closeModal}
+                        >
+                            아니오
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            <div className="p-5 border-b flex justify-between items-center">
+                <p className="text-lg"> 회원탈퇴 </p>
+                <span className="text-end">&gt;</span>
             </div>
         </div>
     )
