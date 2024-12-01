5f63dbce509f3699960c11ed65999a58c449b991
윤효정 <0213yhj@naver.com>
2024-12-01 22:54:06 +0900

    Feat(progress)

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useMemo, useState } from "react";
+import Image from "next/image";
 
 interface Props {
     time: number;
@@ -27,11 +28,31 @@ export default function ProgressBar({time}:Props) {
 
 
   return (
-    <div className="flex flex-col space-y-5 w-full items-center">
-        <div className="bg-slate-100 w-4/5 h-2 rounded-xl">
-            <div className={`bg-emerald-400 h-2 rounded-xl`} style={{width:`${progress}%`}}></div>
+      <div className="flex flex-col space-y-6 w-full items-center">
+        <div className="relative w-4/5">
+          <div className="bg-slate-200 h-[2px]"></div>
+          <div
+            className={`bg-emerald-400 h-[2px] rounded-xl absolute top-0 left-0`}
+            style={{ width: `${progress}%` }}></div>
+          <div className="absolute flex justify-between w-full -top-4">
+            {[0, 50, 100].map((value, index) => (
+              <div
+                key={index}
+                className={`w-8 h-8 flex justify-center items-center rounded-full border-2 ${
+                  progress >= value ? "bg-emerald-400 border-emerald-400" : "bg-white border-slate-200"
+                }`}
+              >
+                {progress >= value && (
+                  <div className="w-7 h-7 bg-emerald-400 rounded-full flex items-center justify-center">
+                    <Image alt="check" src={"/icons/Done_white.png"} width={20} height={20}></Image>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         </div>
-        <div className="text-emerald-500 text-sm">{progress} %</div>
-    </div>
+
+        {/* <div className="text-emerald-500 text-sm">{progress} %</div> */}
+      </div>
   )
 }
--- src/components/progress.tsx
+++ src/components/progress.tsx
@@ -11,18 +11,20 @@ interface Props {
 export default function progress({time}:Props) {
   
   const [info, setInfo] = useState("");
+  let count = 0;
+  const list = ["사업자 등록 인증 완료!", "포스 정보 가져오는 중...", "사업자 계좌 가져오는 중..."]
 
   useEffect(()=>{
-    setInterval
+    setInterval(()=>{count++; setInfo(list[count])},1000)
   },[])
 
   return (
     <div className="container flex flex-col items-center justify-center space-y-8">
 
       <div className="text-lg font-bold">{info} 정보 가져오는 중...</div>
-      <div>
+      {/* <div>
         <Image src={"/progress.gif"} width={140} height={140} alt="progress" unoptimized></Image>
-      </div>
+      </div> */}
       <ProgressBar time={time}/>
     </div>
   )
