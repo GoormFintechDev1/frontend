d330f83de8a632a6ad74816858a13b99b8083bde
윤효정 <0213yhj@naver.com>
2024-12-10 17:37:08 +0900

    Style(report): 리포트 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,10 @@
 'use client'
 
 import Login from "@/components/Login";
-import { useUserInfo } from "@/hooks/useUserQuery";
-import { useRouter } from "next/navigation";
+
 
 export default function LoginPage() {
 
-    const {data:user} = useUserInfo();
-    const router = useRouter();
-    if(user) router.push("/");
-    
     return (
         <div className="container2">
             <Login />
