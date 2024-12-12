c4faefa56e861c9356a16e5e9cb4aa6d8a0d2ecf
윤효정 <0213yhj@naver.com>
2024-12-12 17:34:23 +0900

    Feat(env)

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,9 +1,7 @@
 import { Weather } from "@/interface/weather";
 
-const API_KEY = "923e8dfdb94ace794fe0f0f785023f3e";
-
 export const getWeather = async({lat,long}:Weather) => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`, {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${WEATHER_KEY}`, {
         method: "GET",
       });
     
