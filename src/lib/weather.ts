6093b46e7521248e315958d526fbfd03a8b13f54
carbancle <carbancle2@gmail.com>
2024-12-13 18:10:25 +0900

    FIX(weather): WEATHER_KEY 수정

--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,5 +1,7 @@
 import { Weather } from "@/interface/weather";
 
+const WEATHER_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
+
 export const getWeather = async({lat,long}:Weather) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${WEATHER_KEY}`, {
         method: "GET",
